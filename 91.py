10383187fe3a06de82edb288f9206045a7f90afb
FurlanLucas <84166961+FurlanLucas@users.noreply.github.com>
2022-07-18 00:51:17 -0300

    Graphs update and addition of a lateral load distribution influence analysis in convergency.

--- 91.py
+++ 91.py
@@ -21,7 +21,8 @@ class plate:
     def analysis(self, p0, N, m_max_evaluation, n_max_evaluation,
                  converGraph=False, relError=5e-2, pointType='center',
                  evaluationPoint=[0, 0], typeOfAnalysis='together',
-                 printProcess=False, folderName='fig'):
+                 printProcess=False, folderName='fig', converGraphColor='b',
+                 converGraphName='Convergencia', converGraphLegend=None):
         """Cria a função para a análise da deflexão da placa."""
         print('Iniciando a análise para a placa.')
 
@@ -65,16 +66,21 @@ class plate:
                 k += 1
 
             if converGraph:
-                plt.figure('Convergencia')
-                plt.plot(mn_values[0:k+1], wEval[0:k+1], linewidth=1.5)
+                plt.figure(converGraphName)
+                if converGraphLegend is None:
+                    plt.plot(mn_values[0:k+1], 1e3*wEval[0:k+1], linewidth=1.5,
+                             color=converGraphColor)
+                else:
+                    plt.plot(mn_values[0:k+1], 1e3*wEval[0:k+1], linewidth=1.5,
+                             color=converGraphColor, label=converGraphLegend)
+
                 plt.xlabel('m_max = n_max')
-                plt.ylabel('Deflexão [m]')
+                plt.ylabel('Deflexão [mm]')
                 plt.grid(b=True, which='minor', color='gray', linestyle='-',
                          linewidth=0.1)
                 plt.grid(b=True, which='major', color='gray', linestyle='-',
                          linewidth=0.1)
                 plt.minorticks_on()
-                plt.savefig(folderName + '\\91_convergencia.png')
 
             return self.functionAnalysis(p0, N, e_MAX, e_MAX), e_MAX, e_MAX
 
@@ -113,7 +119,7 @@ a = 1  # [m] Tamanho da placa em x
 b = 1  # [m] Tamanho da placa em y
 t = 0.01  # [m] Espessura da placa
 p0 = 400e3  # [N/m²] Carga transversal distribuída sob a placa
-N = 500e3  # [N/m] Carga lateral distribuída sob a placa
+N0 = 500e3  # [N/m] Carga lateral distribuída sob a placa
 xnum = 100  # Quantidade de pontos em x
 ynum = 100  # Quantidade de pontos em y
 M_MAX = 100  # Número máximo de avaliações de m para a convergência
@@ -123,6 +129,8 @@ P_aval = [a/2, b/2]  # Ponto para avaliar as deflexões (convergência e outros)
 pointsN = 100  # Quantidade de pontos para a análise da variação de N
 Nstart = -1e4  # Valor inicial do intervalo de N para a análise
 Nend = 1e4  # Valor final do intervalo de N para a análise
+Npor = [0.8, 0.9, 1, 1.1, 1.2]  # Proporções de N para a análise na conv.
+colors = ['r', 'b', 'k', 'y', 'g']  # Cores para diferentes gráficos
 
 pointsAB = 101  # Quantidade de pontos para a análise da variação da razão AB
 ABstart = 0.1  # Valor inicial do intervalo de A/B para a análise
@@ -139,9 +147,10 @@ makeFolder(figDirectory)
 X = np.linspace(0, a, xnum)
 Y = np.linspace(0, b, ynum)
 X, Y = np.meshgrid(X, Y)
-w, m_conv, n_conv = myPlate.analysis(p0, N, M_MAX, N_MAX, relError=5e-4,
+w, m_conv, n_conv = myPlate.analysis(p0, N0, M_MAX, N_MAX, relError=5e-4,
                                      converGraph=True, printProcess=True,
                                      evaluationPoint=P_aval)
+plt.savefig(figDirectory + '\\91_convergencia.png')
 Z = np.ndarray(shape=(xnum, ynum), dtype=float, order='F')
 for i in range(xnum):
     for j in range(ynum):
@@ -157,19 +166,32 @@ plt.ylabel('Eixo y [m]')
 ax.set_zlabel('Deflexão z [mm]')
 plt.savefig(figDirectory + '\\91_deflexao3D.png')
 
+# ------------- VARIAÇÃO DA CARGA DISTRIBUIDA N NA CONVERGÊNCIA -------------
+N_new = [N0*i for i in Npor]
+W = np.ndarray(shape=len(Npor))
+for i in range(len(Npor)):
+    w, m_conv, n_conv = myPlate.analysis(p0, N_new[i], M_MAX, N_MAX,
+                                         converGraph=True,
+                                         relError=5e-4,
+                                         converGraphColor=colors[i],
+                                         converGraphName='ConvergenciaN',
+                                         converGraphLegend=str(Npor[i])+' N0')
+    W[i] = w(P_aval[0], P_aval[1])
+plt.legend()
+plt.savefig(figDirectory + '\\91_convergenciaN.png')
+
 # --------------------- VARIAÇÃO DA CARGA DISTRIBUIDA N ---------------------
 print('Iniciando análise da variação da carga N.')
 N_new = np.linspace(Nstart, Nend, pointsN)
 W = np.ndarray(shape=pointsN)
 for i in range(pointsN):
-    w, m_conv, n_conv = myPlate.analysis(p0, N_new[i], M_MAX, N_MAX,
-                                         converGraph=False, printProcess=False)
+    w, m_conv, n_conv = myPlate.analysis(p0, N_new[i], M_MAX, N_MAX)
     W[i] = w(P_aval[0], P_aval[1])
 
 # Gráfico
 fig = plt.figure('VariacaoN')
-plt.plot(N_new, 1e3*W)
-plt.xlabel('Força distribuída N')
+plt.plot(1e-3*N_new, 1e3*W)
+plt.xlabel('Força distribuída N0 [kN]')
 plt.ylabel('Deflexão w [mm]')
 plt.grid(b=True, which='minor', color='gray', linestyle='-', linewidth=0.1)
 plt.grid(b=True, which='major', color='gray', linestyle='-', linewidth=0.1)
@@ -185,8 +207,7 @@ for i in range(pointsAB):
     newb = np.sqrt(A0/ab[i])
     newa = A0/newb
     myPlate = plate(newa, newb, t, mat.Al6061)
-    w, m_conv, n_conv = myPlate.analysis(p0, N, M_MAX, N_MAX,
-                                         converGraph=False, printProcess=False)
+    w, m_conv, n_conv = myPlate.analysis(p0, N0, M_MAX, N_MAX)
     W[i] = w(P_aval[0], P_aval[1])
 
 # Gráfico
