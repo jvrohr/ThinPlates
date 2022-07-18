61edd9ca259ecbee54455b8cecc68b312a94d168
FurlanLucas <84166961+FurlanLucas@users.noreply.github.com>
2022-07-18 00:23:02 -0300

    Graphics adding and figures save process created.

--- 91.py
+++ 91.py
@@ -2,14 +2,11 @@
 
 # Importações
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from matplotlib import cm
 import materials as mat
+import os
 
-# Constantes
-xnum = 100
-ynum = 100
 
 class plate:
     """Cria a classe da placa."""
@@ -22,16 +19,19 @@ class plate:
         self.D = (material.E*t**3)/(1*(1-material.v**2))
 
     def analysis(self, p0, N, m_max_evaluation, n_max_evaluation,
-                 converGraph='False', relError=50e-3, evaluationPoint='center',
-                 xEvaluation=0, yEvaluation=0, typeOfAnalysis='together',
-                 printProcess='False'):
+                 converGraph=False, relError=5e-2, pointType='center',
+                 evaluationPoint=[0, 0], typeOfAnalysis='together',
+                 printProcess=False, folderName='fig'):
         """Cria a função para a análise da deflexão da placa."""
         print('Iniciando a análise para a placa.')
 
-        if evaluationPoint == 'center':
+        if pointType == 'center':
             p = [self.a/2, self.b/2]
+        elif pointType == 'other':
+            p = evaluationPoint
         else:
-            p = [xEvaluation, yEvaluation]
+            print('O valor para pointType ({}) não está definido'
+                  .format(evaluationPoint))
 
         wEval = np.ndarray(shape=m_max_evaluation+1, dtype=float)
         if typeOfAnalysis == 'together':
@@ -41,9 +41,11 @@ class plate:
             E_MAX = 0
             k = 1
             mn_values = np.array(range(int((m_max_evaluation+1)/2)))*2 + 1
-            print('Busca por convergência para Er = {}'.format(relError))
-            print('\tm = n =  1 \tNov aval: {:7.4}'.format((wEval[0])), end='')
-            print(' Aval ant:    -    Erro:     -')
+            print('\tBusca por convergência para Er = {}'.format(relError))
+            if printProcess:
+                print('\tm = n =  1 \t', end='')
+                print('Nov aval: {:7.4}'.format((wEval[0])), end='')
+                print(' Aval ant:    -    Erro:     -')
             for e_MAX in mn_values[1:-1]:
                 w = self.functionAnalysis(p0, N, e_MAX, e_MAX)
                 wEval[k] = w(p[0], p[1])
@@ -64,14 +66,15 @@ class plate:
 
             if converGraph:
                 plt.figure('Convergencia')
-                plt.plot(mn_values[0:k], wEval[0:k], linewidth=2)
+                plt.plot(mn_values[0:k+1], wEval[0:k+1], linewidth=1.5)
                 plt.xlabel('m_max = n_max')
                 plt.ylabel('Deflexão [m]')
                 plt.grid(b=True, which='minor', color='gray', linestyle='-',
-                         linewidth=0.3)
-                plt.grid(b=True, which='major', color='k', linestyle='-',
-                         linewidth=0.8)
+                         linewidth=0.1)
+                plt.grid(b=True, which='major', color='gray', linestyle='-',
+                         linewidth=0.1)
                 plt.minorticks_on()
+                plt.savefig(folderName + '\\91_convergencia.png')
 
             return self.functionAnalysis(p0, N, e_MAX, e_MAX), e_MAX, e_MAX
 
@@ -97,27 +100,104 @@ class plate:
         return w
 
 
-# Trocar
-a = 1
-b = 1
+def makeFolder(name):
+    """Função para criar a pasta de figuras se não existente."""
+    if os.path.isdir(name):
+        return True
+    else:
+        os.mkdir(name)
 
-# Cria o material e a placa
-myPlate = plate(a, b, 0.01, mat.Al6061)
 
-# Parte principal
+# ---------------------- CONSTANTES GERAIS DO PROBLEMA ----------------------
+a = 1  # [m] Tamanho da placa em x
+b = 1  # [m] Tamanho da placa em y
+t = 0.01  # [m] Espessura da placa
+p0 = 400e3  # [N/m²] Carga transversal distribuída sob a placa
+N = 500e3  # [N/m] Carga lateral distribuída sob a placa
+xnum = 100  # Quantidade de pontos em x
+ynum = 100  # Quantidade de pontos em y
+M_MAX = 100  # Número máximo de avaliações de m para a convergência
+N_MAX = 100  # Número máximo de avaliações de n para a convergência
+P_aval = [a/2, b/2]  # Ponto para avaliar as deflexões (convergência e outros)
+
+pointsN = 100  # Quantidade de pontos para a análise da variação de N
+Nstart = -1e4  # Valor inicial do intervalo de N para a análise
+Nend = 1e4  # Valor final do intervalo de N para a análise
+
+pointsAB = 101  # Quantidade de pontos para a análise da variação da razão AB
+ABstart = 0.1  # Valor inicial do intervalo de A/B para a análise
+ABend = 4  # Valor final do intervalo A/B para a análise
+
+figDirectory = 'fig'  # Nome da pasta para salvar as imagens
+
+# ---------------------- ANÁLISE PRINCIPAL DA DEFLEXÃO ----------------------
+# Cria a placa e a pasta para salvas as figuras
+myPlate = plate(a, b, t, mat.Al6061)
+makeFolder(figDirectory)
+
+# Cria os pontos pro gráfico e analisa a placa de entrada
 X = np.linspace(0, a, xnum)
 Y = np.linspace(0, b, ynum)
 X, Y = np.meshgrid(X, Y)
-w, m_conv, n_conv = myPlate.analysis(4, 500e3, 101, 101)
+w, m_conv, n_conv = myPlate.analysis(p0, N, M_MAX, N_MAX, relError=5e-4,
+                                     converGraph=True, printProcess=True,
+                                     evaluationPoint=P_aval)
 Z = np.ndarray(shape=(xnum, ynum), dtype=float, order='F')
 for i in range(xnum):
     for j in range(ynum):
         Z[i][j] = w(X[i][j], Y[i][j])
 
-
-# Gráficos
-fig = plt.figure()
+# Gráfico 3D
+fig = plt.figure('Plot3D')
 ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+surf = ax.plot_surface(X, Y, 1e3*Z, cmap=cm.coolwarm, linewidth=0,
+                       antialiased=True)
+plt.xlabel('Eixo x [m]')
+plt.ylabel('Eixo y [m]')
+ax.set_zlabel('Deflexão z [mm]')
+plt.savefig(figDirectory + '\\91_deflexao3D.png')
+
+# --------------------- VARIAÇÃO DA CARGA DISTRIBUIDA N ---------------------
+print('Iniciando análise da variação da carga N.')
+N_new = np.linspace(Nstart, Nend, pointsN)
+W = np.ndarray(shape=pointsN)
+for i in range(pointsN):
+    w, m_conv, n_conv = myPlate.analysis(p0, N_new[i], M_MAX, N_MAX,
+                                         converGraph=False, printProcess=False)
+    W[i] = w(P_aval[0], P_aval[1])
+
+# Gráfico
+fig = plt.figure('VariacaoN')
+plt.plot(N_new, 1e3*W)
+plt.xlabel('Força distribuída N')
+plt.ylabel('Deflexão w [mm]')
+plt.grid(b=True, which='minor', color='gray', linestyle='-', linewidth=0.1)
+plt.grid(b=True, which='major', color='gray', linestyle='-', linewidth=0.1)
+plt.minorticks_on()
+plt.savefig(figDirectory + '\\91_variacaoN.png')
+
+# -------------------------- VARIAÇÃO DA RAZÃO A/B --------------------------
+print('Iniciando análise da variação da razão AB.')
+ab = np.linspace(ABstart, ABend, pointsAB)
+A0 = a*b  # A variação é feita mantendo a área constante
+W = np.ndarray(shape=pointsAB)
+for i in range(pointsAB):
+    newb = np.sqrt(A0/ab[i])
+    newa = A0/newb
+    myPlate = plate(newa, newb, t, mat.Al6061)
+    w, m_conv, n_conv = myPlate.analysis(p0, N, M_MAX, N_MAX,
+                                         converGraph=False, printProcess=False)
+    W[i] = w(P_aval[0], P_aval[1])
+
+# Gráfico
+plt.figure('VariacaoAB')
+plt.plot(ab, 1e3*W)
+plt.xlabel('Razão a/b')
+plt.ylabel('Deflexão [mm]')
+plt.grid(b=True, which='minor', color='gray', linestyle='-', linewidth=0.1)
+plt.grid(b=True, which='major', color='gray', linestyle='-', linewidth=0.1)
+plt.minorticks_on()
+plt.savefig(figDirectory + '\\91_variacaoAB.png')
+
+# ------------------------------- FINALIZAÇÃO -------------------------------
 plt.show()
